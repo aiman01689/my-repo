b49d8d7d80aa72696a5e111a98e27bac2e5d17bc
aiman01689 <160779331+aiman01689@users.noreply.github.com>
2024-03-11 03:42:52 -0700

    index.ts

--- Assignment/Question19/index.ts
+++ Assignment/Question19/index.ts
@@ -1,4 +1,4 @@
-import{} from '/code';
-
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+let guests: string[] = ["Elon Musk", "Albert Einstein", "Abraham Lincoln"];
 console.log(`n\ PRINTING NUMBER OF GUEST INVITED`)
-console.log(`We had finally invited ${guest_list.length} from Question14` )
+console.log(`We had finally invited ${guests.length} people to dinner`);
